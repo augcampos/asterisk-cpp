86eda15f7fc6ec84179554ae30b8d97f8000613d
Augusto Campos <a-campos@critical-links.com>
2012-05-17 03:20:18 +0100

    change manager include name

---
+++ asterisk-cpp/asteriskcpp/manager/Actions.hpp
@@ -0,0 +1,23 @@
+/*
+ * Actions.hpp
+ *
+ *  Created on: May 17, 2012
+ *      Author: augcampos
+ */
+
+#ifndef ACTIONS_HPP_
+#define ACTIONS_HPP_
+
+#include "asteriskcpp/manager/events/AbsoluteTimeoutAction.h"
+#include "asteriskcpp/manager/events/AbstractManagerAction.h"
+#include "asteriskcpp/manager/events/AbstractMeetMeMuteAction.h"
+#include "asteriskcpp/manager/events/ChallengeAction.h"
+#include "asteriskcpp/manager/events/CommandAction.h"
+#include "asteriskcpp/manager/events/EventsAction.h"
+#include "asteriskcpp/manager/events/ListCommandsAction.h"
+#include "asteriskcpp/manager/events/LoginAction.h"
+#include "asteriskcpp/manager/events/LogoffAction.h"
+#include "asteriskcpp/manager/events/ManagerAction.h"
+
+
+#endif /* ACTIONS_HPP_ */
